e035029079907abbe505aeb755e05028369c4acc
Vadim <vadimchubok@gmail.com>
2025-10-27 23:10:09 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -8,9 +8,7 @@ class Person:
 
 
 def create_person_list(people: list) -> list:
-    person_list = []
-    for person in people:
-        person_list.append(Person(person["name"], person["age"]))
+    person_list = [Person(person["name"], person["age"]) for person in people]
 
     for person in people:
         current_person = Person.people[person["name"]]
